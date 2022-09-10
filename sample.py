ece96927e7b4f49fcbf45070ec278758f394037a
Martin Grunwald <martingrunwald91@gmail.com>
2022-09-10 20:12:56 +0200

    changes in sample.py file

--- sample.py
+++ sample.py
@@ -8,17 +8,24 @@ class MyApp1(Application):
     def onstart(self):
         self.count = 0
 
+
+    def onfinish(self):
+        print("Leaving...")
+
+
+    def exit(self, retval):
+        sys.exit(retval)
+
+
     def main(self):
         cmd = input(f"[{self.count}] To leave enter 'x': ").strip()
         if cmd.lower() in ["x"]:
             return 0
         self.count += 1
 
-    def exit(self, retval):
-        sys.exit(retval)
 
-    def onfinish(self):
-        print("Leaving...")
+
+
 
 
 # this part of code cannot be reached when decorator autorun is used
